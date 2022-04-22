cc7ff9b2192773196362166d079610d6c29d1ac7
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-22 16:29:13 -0400

    added styling +compiled button logic- need to intatiate the button logic to call from Display logic

--- src/Components/DisplayPost/displayPost.jsx
+++ src/Components/DisplayPost/displayPost.jsx
@@ -1,4 +1,3 @@
-
 const DisplayPost = (props) => {
     return (  
         <table>
@@ -13,9 +12,10 @@ const DisplayPost = (props) => {
           {props.parentEntries.map((entry,index) => {
             return (
               <tr key={index}>
-                <td>{index + 1}</td>
                 <td>{entry.name}</td>
+                <li>
                 <td>{entry.post}</td>
+                </li>
                 <td>{entry.date}</td>
               </tr>
             );
